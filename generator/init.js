6711f57469f96c0d9edddc2d21b8efb1b7a2fe5d
james <james.speirs@gmail.com>
2019-04-30 15:00:40 -0600

    add info about icon attribution

--- generator/init.js
+++ generator/init.js
@@ -30,7 +30,7 @@ module.exports = async function (source, { gitOrg = '<git-org>', gitRepository =
         },
         favicon: '/favicon.png',
         finePrint: '<p>This is some mighty find print. Yes, some mighty fine print.</p>' +
-          '<p><strong>NOTE:</strong> Book icon obtained under the <a href="https://creativecommons.org/licenses/by/3.0/" target="_blank">Creative Commons (Attribution 3.0 Unported)</a> license, created by <a href="https://www.iconfinder.com/laurareen">Laura Reen</a>.</p>',
+          '<p><strong>NOTE:</strong> Book icon obtained under the <a href="https://creativecommons.org/licenses/by/3.0/" target="_blank">Creative Commons (Attribution 3.0 Unported)</a> license, created by <a href="https://www.iconfinder.com/laurareen">Laura Reen</a>. So, if you want to keep this icon you need to keep the attribution.</p>',
         footerLinks: [
           { title: 'Github', href: 'https://github.com/${gitOrg}/${gitRepository}' }
         ]
